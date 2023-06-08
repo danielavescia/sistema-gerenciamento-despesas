53018984a005a169639862b643a8ae136768199d
Daniela <danivescia@gmail.com>
2023-06-08 11:41:50 -0300

    Created Conta class with CreateAccout method, ToString

---
+++ Conta.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_Gerenciamento_Despesas
+{
+    internal class Conta
+    {
+        private int id;
+        private string banco, agencia, numeroConta;
+        private double saldo;
+
+        // Propriedades (getters e setters) 
+        public int getId() 
+        {
+            return id;
+        }
+
+        public void setId(int id) 
+        { 
+           this.id = id;
+        }
+
+        public string getBanco()
+        {
+            return banco;
+        }
+
+        public void setBanco(string banco)
+        {
+            this.banco = banco;
+        }
+
+        public string getAgencia()
+        {
+            return agencia;
+        }
+
+        public void setAgencia(string agencia)
+        {
+            this.agencia = agencia;
+        }
+
+        public string getNumeroConta()
+        {
+            return numeroConta;
+        }
+
+        public void setNumeroConta(string numeroConta)
+        {
+            this.numeroConta = numeroConta;
+        }
+
+        public double getSaldo()
+        {
+            return saldo;
+        }
+
+        public void setSaldo(double saldo)
+        {
+            this.saldo = saldo;
+        }
+
+        //Construtor objeto Conta
+        public Conta(int id, string banco, string agencia, string numeroConta, double saldo)
+        {
+            this.id = id;
+            this.banco = banco;
+            this.agencia = agencia;
+            this.numeroConta = numeroConta;
+            this.saldo = saldo;
+
+        }
+       
+        public override string ToString()
+        {
+            return 
+               $@"
+               CONTA {getId}
+               Banco: {getBanco}
+               Agencia: {getAgencia}
+               Conta: {getNumeroConta}
+               Saldo: {getSaldo}
+                ";
+        }
+
+    
+        //método que cria Contas e adiciona a uma Lista de Contas
+        public List<Conta> CriarConta(List<Conta> minhasContas)
+        {
+            String banco = null, agencia = null, numeroConta = null;
+            double saldo = 0;
+            int id = 0;
+
+            Console.WriteLine("Para criar uma conta adicione as informacoes solicitadas abaixo:");
+
+            Console.WriteLine("Digite o nome do seu banco:");
+            banco = Console.ReadLine();
+
+            Console.WriteLine("Agencia:");
+            agencia = Console.ReadLine();
+
+            Console.WriteLine("Numero da conta:");
+            numeroConta = Console.ReadLine();
+
+            id = criaId(minhasContas);
+
+            Conta c = new Conta(id, banco, agencia, numeroConta, saldo);
+
+            minhasContas.Add(c);
+
+            return minhasContas;
+
+        }
+
+        //método que cria um id conforme a quantidade de contas existentes
+        public int criaId(List<Conta> minhasContas) 
+        {
+
+            if (minhasContas == null)
+            {
+                return id = 1;
+            }
+            
+            else
+            {
+                id = minhasContas.Count + 1;
+            }
+            return id;
+        }
+
+
+        public List<Conta> RemoverConta(List<Conta> minhasContas) 
+        {
+            Console.WriteLine("Estas são suas contas ativas");
+
+            foreach (Conta c in minhasContas) 
+            {
+                c.ToString();
+            }
+
+            Console.WriteLine("Digite a id de qual gostaria de remover:");
+        }
+
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,91 @@
+﻿
+class Program
+{
+    static void Main(string[] args) 
+    {
+        //variaveis relacionadas ao menu
+        bool MenuEstaAtivo = true;
+        int opcaoMenu;
+
+        //loop permite a repetição da visualização do menu e a escolha da ação que o usuario deseja executar
+        while (MenuEstaAtivo)
+        {
+
+            Console.WriteLine();
+            ImprimeMenuInicial();
+
+            opcaoMenu = int.Parse(Console.ReadLine());
+
+            //a partir do switch case a opcao desejada permite a execeução do bloco de código
+            switch (opcaoMenu)
+            {
+
+                // cadastrar remover e mesclar contas
+                case 1:
+
+                    Console.WriteLine("____________________________________________");
+                    Console.WriteLine("GERENCIAMENTO DE CONTA");
+                    Console.WriteLine("____________________________________________");
+
+
+                    Console.WriteLine();
+                    break;
+
+                // Exibir e adicionar informações especificas de cada conta como : extrato, adicionar transacoes, transferir fundos, editar ultima transacao
+                case 2:
+
+                    Console.WriteLine("____________________________________________");
+                    Console.WriteLine("GERENCIAR TRANSACOES: ");
+                    Console.WriteLine("____________________________________________");
+
+
+                    break;
+
+
+                case 3:
+
+                   
+                    //Visualizar resumo das contas, resumo da receita e despesa do mês e verificar saldo dos ultimos 6 meses
+                    Console.WriteLine("___________________________________________________________________________________");
+                    Console.WriteLine("PAINEL GERAL: ");
+                    Console.WriteLine("___________________________________________________________________________________");
+   
+
+                    break;
+
+
+                case 4:
+
+                    MenuEstaAtivo = false;
+                    Console.WriteLine("Encerrando o sistema...");
+                    break;
+
+
+                default:
+
+                    Console.WriteLine("Digite uma opcao valida!");
+
+                    break;
+
+            }
+
+        }
+    }
+
+    public static void ImprimeMenuInicial()
+    {
+        Console.WriteLine("=============================== Sistema de Gerencimaneto de Despesas ======================================");
+        Console.WriteLine();
+        Console.WriteLine("=========== MENU INICIAL ===========");
+        Console.WriteLine("1. Gerenciar Contas");
+        Console.WriteLine("2. Gerenciar transações");
+        Console.WriteLine("3. Painel Geral");
+        Console.WriteLine("4. Sair do Sistema");
+        Console.WriteLine();
+        Console.WriteLine("Digite a opção deseja:");
+    }
+}
+
+
+
+
